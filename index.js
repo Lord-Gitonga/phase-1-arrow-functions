c4620953f7fadc829970b9442cb5097f699b23a1
lordish <gitonga403@gmail.com>
2023-03-16 17:07:59 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+// const value = (num1,num2) => num1/num2;
+//  console.log(value(2000,100));
+
+ const divide = function (num1=2000,num2=100){
+    return num1/num2;
+ }
+   
+ console.log(divide());
+
+const square = (num3=3) => {
+    return num3*num3;
+}
+console.log(square());
+
+const add = (num4=2,num5=7) => {
+    return num4 + num5;
+}
+console.log(add());
